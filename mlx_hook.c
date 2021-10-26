c02513cad69dc3ee2e5a80131bdcde0827397210
anclarma <anclarma@student.42.fr>
2021-10-26 02:05:47 +0200

    correction mineur

--- mlx_hook.c
+++ mlx_hook.c
@@ -1,24 +1,24 @@
-/*
-** mlx_hook.c for MiniLibX in 
-** 
-** Made by Charlie Root
-** Login   <ol@epitech.net>
-** 
-** Started on  Thu Aug  3 11:49:06 2000 Charlie Root
-** Last update Fri Jan 28 17:05:28 2005 Olivier Crouzet
-*/
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   mlx_hook.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: Charlie Root <ol@epitech.net>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2000/07/03 11:49:06 by Charlie Root      #+#    #+#             */
+/*   Updated: 2021/10/26 02:05:32 by anclarma         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "mlx_int.h"
 
-
-void	mlx_hook(t_win_list *win, int x_event, int x_mask, 
-		int (*funct)(),void *param)
+void	mlx_hook(t_win_list *win, int x_event, int x_mask,
+	int (*funct)(), void *param)
 {
 	win->hooks[x_event].hook = funct;
 	win->hooks[x_event].param = param;
 	win->hooks[x_event].mask = x_mask;
 }
-
 
 void	mlx_do_key_autorepeatoff(t_xvar *xvar)
 {
@@ -29,7 +29,6 @@ void	mlx_do_key_autorepeaton(t_xvar *xvar)
 {
 	XAutoRepeatOn(xvar->display);
 }
-
 
 void	mlx_do_sync(t_xvar *xvar)
 {
